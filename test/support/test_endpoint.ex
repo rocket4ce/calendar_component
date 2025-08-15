2eaa7eebcf2d320cbfc41b24057d467a798638e0
Dinko Azema <rocket4ce@gmail.com>
2025-08-15 13:44:13 -0400

    Remove unused init function from TestEndpoint module

--- test/support/test_endpoint.ex
+++ test/support/test_endpoint.ex
@@ -2,6 +2,4 @@ defmodule LiveCalendar.TestEndpoint do
   use Phoenix.Endpoint, otp_app: :calendar_component
 
   socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [:peer_data, :x_headers, :uri, :user_agent]])
-
-  def init(_key, config), do: {:ok, config}
 end
